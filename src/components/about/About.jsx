c0c356d0a534fd02dd22e65439f34543beb90ddf
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-07-19 16:19:34 +0530

    Updated About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
 import AboutImg from '../../assets/profile.jpg';
-import CV from '../../assets/John-Cv.pdf';
+import CV from '../../assets/ABHINAV_KULKARNI_RESUME_LATEST.pdf';
 import Info from './Info';
 const About = () => {
   return (
